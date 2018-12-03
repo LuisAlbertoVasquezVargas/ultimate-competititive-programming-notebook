7f1e75b502db4bbb87562ce541bcce6eb49cff20
Luis Vásquez <you@example.cm>
2018-12-03 01:16:28 -0500

    trying text formating config

--- cpp/Graphs/unionFind.cpp
+++ cpp/Graphs/unionFind.cpp
@@ -2,16 +2,18 @@ struct UnionFind {
   vInt parent;
   void initUF(int n) {
     parent = vInt(n);
-    REP(i, n) { parent[i] = i; }
+    REP(i, n) { 
+      parent[i] = i; 
+    }
   }
 
   int find(int x) { 
-		return parent[x] = (parent[x] == x ? x : find(parent[x])); 
-	}
+    return parent[x] = (parent[x] == x ? x : find(parent[x])); 
+  }
 
   void unite(int x, int y) { 
-		parent[find(x)] = find(y); 
-	}
+    parent[find(x)] = find(y); 
+  }
 };
 
 // TODO(luisvasquez) review if it worths to use ranked unionFind. It needs more
--- cpp/Math/primalityTests.cpp
+++ cpp/Math/primalityTests.cpp
@@ -6,31 +6,27 @@ using namespace std;
 typedef long long Long;
 
 bool isPrime(Long n) {
-	if (n == 1) {
-		return false;
-	}
-	if (n == 2) {
-		return true;
-	}
-	if (n % 2 == 0) {
-		return false;
-	}
-	for (int d = 3; (Long)d * d <= n; d += 2) {
-		if (n % d == 0) {
-			return false;
-		}
-	}
-	return true;
+  if (n == 1) {
+    return false;
+  }
+  if (n == 2) {
+    return true;
+  }
+  if (n % 2 == 0) {
+    return false;
+  }
+  for (int d = 3; (Long)d * d <= n; d += 2) {
+    if (n % d == 0) {
+      return false;
+    }
+  }
+  return true;
 }
 
-void test(Long num) { 
-	cout << num << " " << isPrime(num) << '\n'; 
- }
+void test(Long num) { cout << num << " " << isPrime(num) << '\n'; }
 
 int main() {
-	REP(i, 100) { 
-		test(i + 1); 
-	}
-	test(1000000000000000);
-	test((int)1e9 + 7);
+  REP(i, 100) { test(i + 1); }
+  test(1000000000000000);
+  test((int)1e9 + 7);
 }
