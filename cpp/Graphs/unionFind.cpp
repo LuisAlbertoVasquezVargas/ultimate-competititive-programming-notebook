c7b5754617336cdf26ea8f6b0a8ac44de69e3746
Luis Vásquez <you@example.cm>
2018-12-03 00:38:06 -0500

    replacing idents by spaces

--- cpp/Graphs/unionFind.cpp
+++ cpp/Graphs/unionFind.cpp
@@ -1,17 +1,15 @@
 struct UnionFind {
-	vInt parent;
-	void initUF(int n) {
-		parent = vInt(n);
-		REP(i, n) { 
-			parent[i] = i; 
-		}
-	}
+  vInt parent;
+  void initUF(int n) {
+    parent = vInt(n);
+    REP(i, n) { parent[i] = i; }
+  }
 
-	int find(int x) { 
+  int find(int x) { 
 		return parent[x] = (parent[x] == x ? x : find(parent[x])); 
 	}
 
-	void unite(int x, int y) { 
+  void unite(int x, int y) { 
 		parent[find(x)] = find(y); 
 	}
 };
