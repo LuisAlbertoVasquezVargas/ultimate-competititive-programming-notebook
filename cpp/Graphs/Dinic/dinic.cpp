0673fe649f6365da24bb2bc439c182f6ebba09e5
Luis Vásquez <you@example.cm>
2018-12-11 20:22:27 -0500

    minor changes on dinic's algorithm

--- cpp/Graphs/Dinic/dinic.cpp
+++ cpp/Graphs/Dinic/dinic.cpp
@@ -1,22 +1,17 @@
-#define INF (1 << 30)
-#define N 200000
-
-#define DEBUG(x) cout << #x << " " << (x) << endl;
-
-typedef vector<int> vi;
-typedef long long ll;
-
 struct flowGraph {
   int n, m, s, t, E;
-  vi to, cap, NEXT;    // maxe * 2
-  vi last, now, dist;  // maxv
+  const int INF = INT_MAX / 2;
+  vInt to, cap, NEXT;    // maxe * 2
+  vInt last, now, dist;  // maxv
   flowGraph() {}
   flowGraph(int n, int m, int s, int t) : n(n), m(m), s(s), t(t) {
-    to = cap = NEXT = vi(2 * m + 5);
-    last = now = dist = vi(n + 5);
+    // TODO(luisvasquez) : to investigate about NEXT variable.
+    to = cap = NEXT = vInt(2 * m + 5);
+    last = now = dist = vInt(n + 5);
     E = 0;
-    last = vi(n + 5, -1);
+    last = vInt(n + 5, -1);
   }
+
   void add(int u, int v, int uv, int vu = 0) {
     to[E] = v;
     cap[E] = uv;
@@ -28,9 +23,11 @@ struct flowGraph {
     NEXT[E] = last[v];
     last[v] = E++;
   }
+
   bool bfs() {
-    REP (i, n)
+    REP (i, n) {
       dist[i] = INF;
+    }
     queue<int> Q;
     dist[t] = 0;
     Q.push(t);
@@ -47,6 +44,7 @@ struct flowGraph {
     }
     return dist[s] < INF;
   }
+
   int dfs(int u, int f) {
     if (u == t) return f;
     for (int &e = now[u]; e != -1; e = NEXT[e]) {
@@ -62,14 +60,17 @@ struct flowGraph {
     }
     return 0;
   }
-  ll maxFlow() {
-    ll flow = 0;
+  Long maxFlow() {
+    Long flow = 0;
     while (bfs()) {
-      REP (i, n)
+      REP (i, n) {
         now[i] = last[i];
-      while (1) {
+      }
+      while (true) {
         int f = dfs(s, INF);
-        if (!f) break;
+        if (!f) {
+          break;
+        }
         flow += f;
       }
     }
