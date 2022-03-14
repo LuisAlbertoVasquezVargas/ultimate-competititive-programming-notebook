f544d24a0fcb953d8f865cae2b9311027c8684e0
luisvasquez <luisv0909@gmail.com>
2022-03-13 20:02:07 -0500

    added ckmin & ckmax templates

--- template.cpp
+++ template.cpp
@@ -10,3 +10,6 @@ typedef vector<Pair> vPair;
 #define ALL(v) v.begin(), v.end()
 #define SZ(v) (int)v.size()
 #define PB push_back
+
+template<class T> bool ckmin(T& a, const T& b) { return b < a ? a = b, 1 : 0; }
+template<class T> bool ckmax(T& a, const T& b) { return a < b ? a = b, 1 : 0; }
