94fedff4987b2ce7507f8c032d6a3cd6570be82e
luisvasquez <luisv0909@gmail.com>
2022-03-17 14:27:21 -0500

    added codeforces script for folder creation

---
+++ scripts/createCDFcontest.sh
@@ -0,0 +1,10 @@
+contestname="CDF_DIV3_123";
+mkdir ${contestname}
+cp ../templates/codeforces.cpp "./${contestname}/A.cpp"
+cp ../templates/codeforces.cpp "./${contestname}/B.cpp"
+cp ../templates/codeforces.cpp "./${contestname}/C.cpp"
+cp ../templates/codeforces.cpp "./${contestname}/D.cpp"
+cp ../templates/codeforces.cpp "./${contestname}/E.cpp"
+cp ../templates/codeforces.cpp "./${contestname}/F.cpp"
+cp ../templates/codeforces.cpp "./${contestname}/G.cpp"
+cp ../templates/codeforces.cpp "./${contestname}/in.txt"
