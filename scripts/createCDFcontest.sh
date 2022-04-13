07dfad50db9da00ecfebc159c8bb3b2abca62b64
luisvasquez <luisv0909@gmail.com>
2022-04-13 00:01:06 -0500

    fixed macro slash not printing endline & fixed issue with template creation where txt files were created with content

--- scripts/createCDFcontest.sh
+++ scripts/createCDFcontest.sh
@@ -1,4 +1,4 @@
-contestname="CDF_DIV3_773";
+contestname="CDF_DIV3_123456";
 mkdir ${contestname}
 cp ../templates/codeforces.cpp "./${contestname}/A.cpp"
 cp ../templates/codeforces.cpp "./${contestname}/B.cpp"
@@ -7,4 +7,4 @@ cp ../templates/codeforces.cpp "./${contestname}/D.cpp"
 cp ../templates/codeforces.cpp "./${contestname}/E.cpp"
 cp ../templates/codeforces.cpp "./${contestname}/F.cpp"
 cp ../templates/codeforces.cpp "./${contestname}/G.cpp"
-cp ../templates/codeforces.cpp "./${contestname}/in.txt"
+touch "./${contestname}/in.txt"
